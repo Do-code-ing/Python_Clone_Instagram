09397cb32707c9cb5e7bf3382e22f43994c12e49
Do-code-ing <zxcvbn658@naver.com>
2021-10-11 23:15:37 +0900

    2021-10-11

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from instagram.models import *
 
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(Image)
+admin.site.register(Like)
+admin.site.register(Follow)
+admin.site.register(Comment)
--- instagram/models.py
+++ instagram/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.deletion import CASCADE
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
@@ -10,4 +10,24 @@ class Post(models.Model):
 
 
 class Image(models.Model):
-    Post = models.ForeignKey(Post, on_delete=CASCADE)
+    Post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="image", blank=True, null=True)
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+
+class Follow(models.Model):
+    follower = models.ForeignKey(
+        User, related_name="follower", on_delete=models.CASCADE)
+    following = models.ForeignKey(
+        User, related_name="following", on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    text = models.CharField(max_length=100)
+    create_date = models.DateTimeField(auto_now_add=True)
